46d235c166bcbf13f342716b4ccbe541836192a5
Pangpond543 <pond543543@gmail.com>
2026-07-07 18:03:40 +0700

    efefew

---
+++ Lecture-02/Lecture-02/test_input2.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = input("Enter your income are")
+income = float(input('What is your income? '))
+
+print('Here is the data you entered')
+print('Name: ' + name)
+print('Age: ' + age)
+print('Income: ', format(income, '12,.2f'))
