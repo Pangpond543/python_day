8acd558ec1bfc7e50e4fae62e31013f8d3fad094
Pangpond543 <pond543543@gmail.com>
2026-07-23 14:50:20 +0700

    urtrhfg

--- Lecture-04/Calculating_Running_total.py
+++ Lecture-04/Calculating_Running_total.py
@@ -1,10 +1,17 @@
 numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
-sum = 0
+sum_even = 0
+sum_odd = 0
 
 for val in numbers:
-    sum += val
-    print(sum)
+    if val % 2 == 0:
+        sum_even += val
+        print("list even",val)
     
-print("The sum is", sum)
+    if val % 2 == 1:
+        sum_odd += val
+        print("list odd",val)
+    
+print("The sum even is", sum_even)
+print("The sum odd is", sum_odd)
 
 #รวมเฉพาะเลขคู่กับเลขคี่ โจทย์ต่อ
