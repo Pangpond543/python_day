4da2add3a788f3917186dc803b2527e23a598fae
Pangpond543 <pond543543@gmail.com>
2026-08-11 16:57:26 +0700

    Exa

--- Lecture-05/Exercise.py
+++ Lecture-05/Exercise.py
@@ -1,12 +1,15 @@
 def is_armstong(number):
     digits = len(str(number))
+    print(digits)
     total = 0
-    for sum in [str(number)]:
-        total += sum ** digits
+    for sum in str(number):
+        total += int(sum) ** digits
+        print(total)
 
     if total == number:
         return True
     else:
         return False
+    
     
 print(is_armstong(153))
--- Lecture-05/Exercise2.py
+++ Lecture-05/Exercise2.py
@@ -1,6 +1,15 @@
 def generate_primes(number):
-    for i in range(1,number):
-        if i % number == 0:
-            print(i)
-
-generate_primes(10)
+    num = ()
+    for i in range(2, number + 1):
+        cout = 0
+        
+        
+        for j in range(1, i + 1):
+            if i % j == 0:
+                cout += 1
+                
+        if cout == 2:
+            num += i,
+    return num
+            
+print(generate_primes(10))
