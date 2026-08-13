e6da264aae91bfec430cb7bcad064d325c0181c2
Pangpond543 <pond543543@gmail.com>
2026-08-13 11:12:30 +0700

    lecture-06

---
+++ Lecture-06/lsit_1.py
@@ -0,0 +1,5 @@
+prime_numbers = [2, 3, 5, 7, 11, 17, 19, 23, 29]
+print(f'Prime_numbers: {prime_numbers})
+      
+fifth_prime = prime_numbers[4]
+print(f"Fith prime number: {fifth_priime}")
---
+++ Lecture-06/modifting_list.py
@@ -0,0 +1,4 @@
+shapes = ["circle", "square", "triangle", "rectangle", "hexagon"]
+shapes[1] = "elipse"
+shapes[3] = "pentagon"
+print(f"Modified shapes: {shapes}")
