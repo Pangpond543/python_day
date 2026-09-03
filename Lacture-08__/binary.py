3d55cbb8772661d533125535f9a12ec4de9a5b9f
Pangpond543 <pond543543@gmail.com>
2026-09-03 08:20:05 +0700

    up

--- Lacture-08__/binary.py
+++ Lacture-08__/binary.py
@@ -5,5 +5,5 @@ with open("records.bin", "rb") as file:
 
     record = struct.unpack('i20sif', data)
 
-    record = (record[0], record[1].decoder().strip('\x00'), record[2], record[3])
-    print(f"ID: {record[0], Name: {record[1]}, Age: {record[2]}, GPA: {record[3]}}")
+    record = (record[0], record[1].decode().strip('\x00'), record[2], record[3])
+    print(f"ID: {record[0]}, Name: {record[1]}, Age: {record[2]}, GPA: {record[3]}")
